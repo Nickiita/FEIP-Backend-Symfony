900fdd5e394b09a1bbea7f6c308764832449a313
Nickiita <feklinnikita@gmail.com>
2025-06-30 23:16:19 +1000

    Fix test controllers

--- tests/Controller/HouseControllerTest.php
+++ tests/Controller/HouseControllerTest.php
@@ -2,33 +2,81 @@
 
 namespace App\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use PHPUnit\Framework\MockObject\Exception;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use App\Service\CsvService;
-
-#[Route('/api/houses', methods: ['GET'])]
-class HouseControllerTest extends AbstractController
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use PHPUnit\Framework\MockObject\MockObject;
+class HouseControllerTest extends WebTestCase
 {
-    public function __construct(
-        private readonly CsvService $csvService,
-        private readonly string $filename
-    ) {}
+    private KernelBrowser $client;
+    private CsvService|MockObject $csvServiceMock;
 
-    public function __invoke(): JsonResponse
+    /**
+     * @throws Exception
+     */
+    protected function setUp(): void
     {
+        $this->client = static::createClient();
 
-        $lines = $this->csvService->readAll($this->filename);
+        $this->csvServiceMock = $this->createMock(CsvService::class);
 
-        $headers = array_shift($lines) ?? [];
+        $container = $this->client->getContainer();
+        $container->set('App\Service\CsvService', $this->csvServiceMock);
+    }
 
-        $data = [];
-        foreach ($lines as $row) {
-            if (count($row) !== count($headers)) {
-                continue;
-            }
-            $data[] = array_combine($headers, $row);
-        }
-        return $this->json($data);
+    public function testListHouses(): void
+    {
+        $this->csvServiceMock->expects($this->once())
+            ->method('readAll')
+            ->willReturn([
+                ['id', 'name', 'price'],
+                [1, 'House One', 100],
+                [2, 'House Two', 200]
+            ]);
+
+        $this->client->request('GET', '/api/houses');
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('Content-Type', 'application/json');
+
+        $expectedResponse = [
+            ['id' => '1', 'name' => 'House One', 'price' => '100'],
+            ['id' => '2', 'name' => 'House Two', 'price' => '200'],
+        ];
+
+        $this->assertEquals(
+            $expectedResponse,
+            json_decode($this->client->getResponse()->getContent(), true)
+        );
+    }
+
+    public function testListHousesWhenFileEmpty(): void
+    {
+        $this->csvServiceMock->expects($this->once())
+            ->method('readAll')
+            ->willReturn([]);
+
+        $this->client->request('GET', '/api/houses');
+        $this->assertResponseIsSuccessful();
+
+        $this->assertEquals(
+            [],
+            json_decode($this->client->getResponse()->getContent(), true)
+        );
+    }
+
+    public function testListHousesWhenFileNotExists(): void
+    {
+        $this->csvServiceMock->expects($this->once())
+            ->method('readAll')
+            ->willReturn([]);
+
+        $this->client->request('GET', '/api/houses');
+        $this->assertResponseIsSuccessful();
+
+        $this->assertEquals(
+            [],
+            json_decode($this->client->getResponse()->getContent(), true)
+        );
     }
 }
